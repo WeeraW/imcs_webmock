085c486da5732997e7ebecd76dfa0cc0cfa381eb
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-07-22 10:22:15 +0700

    fix factory tha cause test fail

--- spec/factories/order_orders.rb
+++ spec/factories/order_orders.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :order_order, class: 'Order::Order' do
     sequence(:billing_id) { |n| "bill-id-#{n}" }
     paid_full_date 1.day.ago.middle_of_day
-    create_by_staff nil
-    paid_approve_by_staff nil
-    shipping_approve_by_staff nil
+    association :create_by, factory: :staff
+    association :paid_approve_by, factory: :staff
+    association :shipping_approve_by, factory: :staff
   end
 end
