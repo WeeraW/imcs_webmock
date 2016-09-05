a105c259a94016677b7a864ededa4d538f64f0e2
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-05 15:27:57 +0700

    fix inventory not allocate itemlot

--- app/controllers/staffs/warehouses/inventory_movements_controller.rb
+++ app/controllers/staffs/warehouses/inventory_movements_controller.rb
@@ -8,79 +8,69 @@ class Staffs::Warehouses::InventoryMovementsController < ApplicationController
   def create
     flash['warning'] = []
     @succeed_allocate_order_ids = []
-    @orders_allocate_hash = {}
     @orders.each do |order|
       next unless order.shipping_approve_by.nil?
-      order_detail_inventory_hash = generate_inventory_allocate_hash(order)
-      if order_detail_inventory_hash
-        @orders_allocate_hash.store(order.id, order_detail_inventory_hash)
+      allocated_item_lots_by_order_detail = inventory_allocate(order)
+      if allocated_item_lots_by_order_detail
         @succeed_allocate_order_ids.append(order.id)
         shipping_approve!(order)
+        save_stock_out_and_stock_out_history!(allocated_item_lots_by_order_detail)
       else
         flash['warning'].append("Inventory Items in storage is not enough for Billng ID: #{order.billing_id}")
       end
     end
-    logger.info "Order Inventory Allocate Hash: #{@orders_allocate_hash.inspect}"
-    allocate_inventory_orders
     redirect_to_render_order_receipt
   end
 
   private
+
+  def save_stock_out_and_stock_out_history!(allocated_item_lots_by_order_detail)
+    allocated_item_lots_by_order_detail.each_pair do |order_detail_id, stock_out_instances|
+      stock_out_instances.each do |stock_out_instance|
+        stock_out_instance.save!
+        Inventory::OrderStockOutHistory.create!(order_detail_id: order_detail_id, inventory_item_lot_stock_out: stock_out_instance)
+      end
+    end
+  end
 
   def shipping_approve!(order)
     order.shipping_approve_by = current_staff
     order.save!
   end
 
-  # TODO: Need to refactoring!!
-  # hash format { <order detail id> => { <inventory item id> => <quantity>, ... }, ... }
-  def generate_inventory_allocate_hash(order)
-    order_details_allocate_hash = {}
+  def inventory_allocate(order)
+    order_stock_out = {}
     order.order_details.each do |order_detail|
-      allocate_hash = {}
+      order_stock_out[order_detail.id] = []
+      allocate_inventory_lots = []
       order_detail.product.contains.each do |product_contain|
-        allocate_hash = generate_inventory_intem_and_quantity_hash(product_contain, order_detail.quantity * product_contain.quantity)
+        allocate_inventory_lots.concat allocate_lot_by_inventory_item(product_contain, order_detail.quantity * product_contain.quantity)
       end
-      return false unless allocate_hash
-      order_details_allocate_hash.store(order_detail.id, allocate_hash)
+      return false unless allocate_inventory_lots
+      order_stock_out[order_detail.id].concat allocate_inventory_lots
     end
-    order_details_allocate_hash
+    order_stock_out
   end
 
-  def generate_inventory_intem_and_quantity_hash(product_contain, inventory_allocate_quantity)
+  def allocate_lot_by_inventory_item(product_contain, inventory_allocate_quantity)
     return false if inventory_allocate_quantity > product_contain.inventory_item.available
-    inventory_allocate_hash = {}
-    inventory_allocate_hash.store(product_contain.inventory_item.id, inventory_allocate_quantity)
-    inventory_allocate_hash
-  end
-
-  def allocate_inventory_orders
-    @orders_allocate_hash.each_value do |order_detail_hash|
-      allocate_inventory(order_detail_hash)
-    end
-  end
-
-  # TODO: Need to refactoring!!
-  def allocate_inventory(order_detail_hash)
-    order_detail_hash.each_pair do |order_detail_id, inventory_item_allocate_hash|
-      inventory_item_allocate_hash.each_pair do |inventory_item_id, allocate_quantity|
-        inventory_item = Inventory::InventoryItem.find(inventory_item_id)
-        inventory_item.available_lots.each do |lot|
-          if lot.available > allocate_quantity
-            allocate_lot(lot, order_detail_id, allocate_quantity)
-            break
-          else
-            allocate_quantity -= lot.available
-            allocate_lot(lot, order_detail_id, lot.available)
-          end
-        end
+    allocated_lots = []
+    inventory_item = Inventory::InventoryItem.find(product_contain.inventory_item.id)
+    inventory_item.available_lots.each do |lot|
+      if lot.available > inventory_allocate_quantity
+        allocated_lots.append buld_allocate_lot(lot, inventory_allocate_quantity)
+        break
+      else
+        inventory_allocate_quantity -= lot.available
+        allocated_lots.append buld_allocate_lot(lot, lot.available)
       end
     end
+    allocated_lots
   end
 
-  def allocate_lot(lot, order_detail_id, allocate_quantity)
-    outstock_item = lot.outstock_items.create!(quantity: allocate_quantity)
-    Inventory::OrderStockOutHistory.create!(order_detail_id: order_detail_id, inventory_item_lot_stock_out: outstock_item)
+  def buld_allocate_lot(lot, allocate_quantity)
+    outstock_item = lot.outstock_items.new(quantity: allocate_quantity)
+    outstock_item
   end
 
   def redirect_to_render_order_receipt
