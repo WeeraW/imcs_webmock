eb632cc1233dd87adfc6ea79f916033d71c8ce25
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-07 17:11:17 +0700

    add report staff commissions

---
+++ app/controllers/staffs/reports/staff_commissions_controller.rb
@@ -0,0 +1,22 @@
+class Staffs::Reports::StaffCommissionsController < ApplicationController
+  before_action :authenticate_staff!
+  before_action :find_paid_orders_in_date_range!, only: [:index]
+  def index
+  end
+
+  private
+
+  def find_paid_orders_in_date_range!
+    @begin_date = params[:beginning_date].blank? ? Date.today.beginning_of_month : Date.parse(params[:beginning_date])
+    @end_date = params[:end_date].blank? ? Date.today.end_of_month : Date.parse(params[:end_date])
+    @staff_orders = Staff.joins(:created_orders).group(:id).where(['order_orders.paid_approve_by_staff_id IS NOT ?', nil]).where(created_at: @begin_date..@end_date)
+
+    respond_staff_comission_report
+  end
+
+  def respond_staff_comission_report
+    respond_to do |format|
+      format.html { render 'index' }
+    end
+  end
+end
