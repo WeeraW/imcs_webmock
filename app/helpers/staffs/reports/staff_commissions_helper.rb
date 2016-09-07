3210e10851a5f381e8ead0c267f0eb254dd5d242
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-07 17:13:04 +0700

    add report staff commissions (เิ่มเติ่ม)

---
+++ app/helpers/staffs/reports/staff_commissions_helper.rb
@@ -0,0 +1,2 @@
+module Staffs::Reports::StaffCommissionsHelper
+end
---
+++ spec/helpers/staffs/reports/staff_commissions_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Staffs::Reports::StaffCommissionsHelper. For example:
+#
+# describe Staffs::Reports::StaffCommissionsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe Staffs::Reports::StaffCommissionsHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
