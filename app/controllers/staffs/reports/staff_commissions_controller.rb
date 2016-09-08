80d6adf453f7393eaea7b66a78ff724b3c08e0af
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-08 10:49:40 +0700

    commission report improvments and add staff active checkbox to staff form

--- app/controllers/staffs/reports/staff_commissions_controller.rb
+++ app/controllers/staffs/reports/staff_commissions_controller.rb
@@ -9,7 +9,7 @@ class Staffs::Reports::StaffCommissionsController < ApplicationController
   def find_paid_orders_in_date_range!
     @begin_date = params[:beginning_date].blank? ? Date.today.beginning_of_month : Date.parse(params[:beginning_date])
     @end_date = params[:end_date].blank? ? Date.today.end_of_month : Date.parse(params[:end_date])
-    @staff_orders = Staff.joins(:created_orders).group(:id).where(['order_orders.paid_approve_by_staff_id IS NOT ?', nil]).where(created_at: @begin_date..@end_date)
+    @staff_orders = Staff.joins(:created_orders).group(:id).where(['order_orders.paid_approve_by_staff_id IS NOT ?', nil]).where(created_at: @begin_date..@end_date).page(params[:page]).per(params[:per_page])
 
     respond_staff_comission_report
   end
