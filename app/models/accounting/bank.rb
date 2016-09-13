805a4142f040b751273e3d141333edae5ef73e32
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-13 10:17:49 +0700

    fix bank association has Very Very Wrog Reference

--- app/models/accounting/bank.rb
+++ app/models/accounting/bank.rb
@@ -1,5 +1,5 @@
 class Accounting::Bank < ApplicationRecord
-  has_many :bank_branch, class_name: 'Accounting::BankBranch', foreign_key: :accounting_bank_branch_id
+  has_many :bank_branches, class_name: 'Accounting::BankBranch', foreign_key: :accounting_bank_id
   has_many :company_accounts, class_name: 'Accounting::CompanyBankAccount', through: :bank_branch
 
   validates :code,
