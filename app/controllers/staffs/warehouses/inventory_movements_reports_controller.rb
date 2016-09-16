5894687d2fd942d8779506f40c2df288977bd7e8
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-16 11:55:52 +0700

    fix bugs

--- app/controllers/staffs/warehouses/inventory_movements_reports_controller.rb
+++ app/controllers/staffs/warehouses/inventory_movements_reports_controller.rb
@@ -1,7 +1,7 @@
 class Staffs::Warehouses::InventoryMovementsReportsController < ApplicationController
   def inventory_movements
-    @begin_date = DateTime.now.beginning_of_day
-    @end_date = DateTime.now.end_of_day
+    @begin_date = params[:begin_date].blank? ? Date.today.beginning_of_month : Date.parse(params[:begin_date])
+    @end_date = params[:end_date].blank? ? Date.today.end_of_month : Date.parse(params[:end_date])
     @inventory_movements = (Inventory::ItemLotStockOut.includes(inventory_item_lot: [:inventory_item], stock_out_history: [order_detail: [:order]]).where(created_at: @begin_date..@end_date) + Inventory::ItemLotStockIn.includes(inventory_item_lot: [:inventory_item]).where(created_at: @begin_date..@end_date)).sort_by { |e| e.created_at }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,6 @@ Rails.application.routes.draw do
       resources :orders, only: [:index, :new, :create, :edit, :update]
     end
   end
-  # get '*not_found' => 'application#render_not_found'
+  get '*not_found' => 'application#render_not_found'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
