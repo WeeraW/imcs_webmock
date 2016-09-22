e93a69740f61666e74affcb99786087a857a685e
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-09-22 09:10:35 +0700

    fix skip bullet when in delopment mode only

--- app/controllers/staffs/sales/fullfilled_orders_controller.rb
+++ app/controllers/staffs/sales/fullfilled_orders_controller.rb
@@ -1,6 +1,6 @@
 class Staffs::Sales::FullfilledOrdersController < ApplicationController
   before_action :authenticate_staff!
-  around_action :skip_bullet, only: [:index]
+  around_action :skip_bullet, only: [:index], if: -> { Rails.env.eql? 'development' }
   def index
     @orders = Order::Order.fullfilled_orders_for_sale(current_staff.id).page(params[:page]).per(params[:per_page])
   end
