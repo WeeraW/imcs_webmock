3f05abd66f6e33a786932ffb7885c0d05b5ff1ab
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-10-31 14:53:06 +0700

    query optimize

--- app/controllers/staffs/reports/staff_commissions_controller.rb
+++ app/controllers/staffs/reports/staff_commissions_controller.rb
@@ -10,7 +10,12 @@ class Staffs::Reports::StaffCommissionsController < ApplicationController
   def find_paid_orders_in_date_range!
     @begin_date = params[:begin_date].blank? ? Date.today.beginning_of_month : Date.parse(params[:begin_date])
     @end_date = params[:end_date].blank? ? Date.today.end_of_month : Date.parse(params[:end_date])
-    @staff_orders = Staff.joins(:sale_orders).group(:id).where(['order_orders.paid_approve_by_staff_id IS NOT ?', nil]).where(['order_orders.paid_full_date BETWEEN ? AND ?', @begin_date, @end_date]).page(params[:page]).per(params[:per_page])
+    @staff_total_sales =
+      Staff.select(:id, :employee_code, :first_name, :last_name, 'SUM(payment_details.pay_amount_reconciled) AS total_sale')
+           .joins(sale_orders: [:payment_details])
+           .group(:id)
+           .where(['order_orders.paid_approve_by_staff_id IS NOT ?', nil])
+           .where(['order_orders.paid_full_date BETWEEN ? AND ?', @begin_date, @end_date]).page(params[:page]).per(params[:per_page])
   end
 
   def respond_staff_comission_report
