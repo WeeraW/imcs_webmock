21111b39aac269f6e7d382abc583aebdfaddcf46
Weerapong Wisomka <teendoi.sp@gmail.com>
2016-11-01 09:28:09 +0700

    fix query scope

--- app/controllers/staffs/sales/fullfilled_orders_controller.rb
+++ app/controllers/staffs/sales/fullfilled_orders_controller.rb
@@ -19,14 +19,23 @@ class Staffs::Sales::FullfilledOrdersController < ApplicationController
   private
 
   def search_orders
-    @orders = Order::Order.includes(order_details: [:product], shipping_address: [:freight_provider])
-                          .search_by_full_name_or_billing_id(params[:search_key])
-                          .fullfilled_orders_for_sale(current_staff.id)
-                          .page(params[:page])
-                          .per(params[:per_page])
+    @orders =
+      Order::Order.includes(order_details: [:product], shipping_address: [:freight_provider])
+                  .search_by_full_name_or_billing_id(params[:search_key])
+                  .fullfilled_orders_for_sale(current_staff.id)
+                  .where('shipping_approve_by_staff_id IS NOT ?', nil)
+                  .order(id: :desc)
+                  .page(params[:page])
+                  .per(params[:per_page])
   end
 
   def find_fullfilled_orders
-    @orders = Order::Order.includes(order_details: [:product], shipping_address: [:freight_provider]).fullfilled_orders_for_sale(current_staff.id).page(params[:page]).per(params[:per_page])
+    @orders =
+      Order::Order.includes(order_details: [:product], shipping_address: [:freight_provider])
+                  .fullfilled_orders_for_sale(current_staff.id)
+                  .where('shipping_approve_by_staff_id IS NOT ?', nil)
+                  .order(id: :desc)
+                  .page(params[:page])
+                  .per(params[:per_page])
   end
 end
